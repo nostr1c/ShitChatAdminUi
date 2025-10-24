df0c7b20b1515b53421fe007510f26606c118629
Filip Siri <filipsiri03@hotmail.com>
2025-10-24 13:52:35 +0200

    api url fix

--- src/Api/axiosInstance.ts
+++ src/Api/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+const BASE_URL = "https://api.filipsiri.se/api/v1";
+// const BASE_URL = "http://localhost:8080/api/v1";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
